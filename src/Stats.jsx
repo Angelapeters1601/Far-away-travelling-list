9e64842cb8522757fefcd82d0408f99d78d7670d
Angelapeters1601 <nwattaha@gmail.com>
2024-07-08 12:37:10 +0200

    Simple layout

---
+++ src/Stats.jsx
@@ -0,0 +1,11 @@
+function Stat() {
+  return (
+    <div className="footer">
+      <footer className="stats">
+        <em>💼You have X items on your list, and you already packed X (X%)</em>
+      </footer>
+    </div>
+  );
+}
+
+export default Stat;
